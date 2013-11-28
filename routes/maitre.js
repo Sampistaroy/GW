d59a0f7843ea7cd40938e3be01d41a69693703d0
SAmpiiero <SAmpiiero@gmail.com>
2013-11-28 01:27:08 +0100

    conception interface maitre du jeu en cours

--- routes/maitre.js
+++ routes/maitre.js
@@ -13,6 +13,26 @@ module.exports = function (app){
 		});
 	});
 
+/*
+ * GET Aide.
+ */
+	app.get('/maitre-aide', ensureAuthenticated, function (req, res){
+		res.render('maitre/aide', {
+			user : req.user,
+			title : 'Mais biensur que vous le savez déjà Maitre'
+		});
+	});
+
+/*
+ * GET New unit.
+ */
+	app.get('/maitre-unit-new', ensureAuthenticated, function (req, res){
+		res.render('unit/new', {
+			user : req.user,
+			title : ' Que voulez vous créer Maitre Du Jeu'
+		});
+	});
+
 
 
 };
